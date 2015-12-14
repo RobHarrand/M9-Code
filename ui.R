d9bd5c9aacb614f62974e9f05a36765e99b18256
Rob Harrand <rob@avacta.com>
2015-12-14 10:31:45 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 shinyUI(fluidPage(
-  titlePanel("Titanic Prediction Applet"),
+  titlePanel("Titanic Prediction Applet"), #Create the title
   
+  #Create the inputs on a side panel,
   sidebarLayout(
     sidebarPanel(
         radioButtons("Pclass", label = "Passenger Class", choices = list("1st Class" = 1, "2nd Class" = 2,
@@ -8,6 +9,7 @@ shinyUI(fluidPage(
         radioButtons("Sex", label = "Sex", choices = list("Male" = 1, "Female" = 0), selected = 1),
         numericInput("Age", label = "Age", value = 0, min = 0, max = 150),
 
+#Create the calcualte button,
       column(3,
              h4("Survival Calculation"),
              actionButton("action", label = "Calculate"))
@@ -15,6 +17,7 @@ shinyUI(fluidPage(
       
     ),
     
+    #Output the picture and prediction result,
     mainPanel(
       img(src="pic.png", height = 96, width = 300),
       textOutput("text1")
